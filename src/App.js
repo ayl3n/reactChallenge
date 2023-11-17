1390f4fe60e14b2744a2958dcc0327805ce340f0
aylen luquez <aylen968@gmail.com>
2023-11-17 00:21:03 -0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inventory, setInventory] = useState([
+    { id: 1, name: 'Product A', quantity: 0, price: 10.00 },
+    { id: 2, name: 'Product B', quantity: 0, price: 20.00 },
+  ]);
+
+  const increaseQuantity = (id) => {
+    setInventory((prevInventory) =>
+      prevInventory.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+      )
+    );
+  };
+
+  const decreaseQuantity = (id) => {
+    setInventory((prevInventory) =>
+      prevInventory.map((item) =>
+        item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+      )
+    );
+  };
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className= "inventory-table">
+        <thead>
+          <tr className='table-elem'>
+            <th>Cantidad</th>
+            <th>Descripcion</th>
+            <th>Subtotal</th>
+            <th>Total</th>
+            <th>Quitar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {inventory.map((item) => (
+            <tr key={item.id} >
+              <td>
+              <button onClick={() => decreaseQuantity(item.id)}>-</button>
+              {item.quantity}
+              <button onClick={() => increaseQuantity(item.id)}>+</button>
+              </td>
+              <td>{item.name}</td>
+              <td>${item.price.toFixed(2)}</td>
+              <td>${(item.price*item.quantity).toFixed(2)}</td>
+              
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
